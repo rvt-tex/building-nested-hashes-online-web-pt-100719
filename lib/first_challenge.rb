8393809806386e5a0f428b4c229097adeb129a28
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 22:55:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,18 +5,3 @@ def first_challenge
    }
 end
 
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      },
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-      }}
-end
