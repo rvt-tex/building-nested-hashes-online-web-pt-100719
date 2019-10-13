94879a191a5a8919860eb6c7466ecba3a0c49c20
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 22:54:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,11 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
+#def second_challenge
+  #epic_tragedy = {
+    #:montague => {
       #your key/value pairs here!
-      }, 
-    :capulet => {
+     # }, 
+   # :capulet => {
       #your key/value pairs here!
-      }}
+    #  }}
 
   
 end
